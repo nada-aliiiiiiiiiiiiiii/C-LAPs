cad608f2b2f129cf1f14e17e39f26585b03e441d
nada-aliiiiiiiiiiiiiii <120502506+nada-aliiiiiiiiiiiiiii@users.noreply.github.com>
2025-10-07 18:48:34 +0300

    LAP6

---
+++ LAP6/LAP6/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace LAP6
+{
+    internal class Program
+    {
+        public static void myfun(int x, int y)
+        {
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("invalid format ");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("invalid number ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            Console.WriteLine("Please enter number 1 :");
+            bool z = int.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("Please enter number 2 :");
+            bool f = int.TryParse(Console.ReadLine(), out y);
+
+            if (z == true && f == true)
+            {
+                myfun(x, y);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+            int[] arr ={10,40,23,45,74 };
+            try
+            {
+                Console.WriteLine(arr[3]);
+
+                Console.WriteLine(arr[5]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            using (X X1 = new X(10, 20))
+            {
+
+            }
+
+        }
+    }
+}
---
+++ LAP6/LAP6/X.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAP6
+{
+    internal class X: IDisposable
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public X(int _n1, int _n2)
+        {
+            num1 = _n1;
+            num2 = _n2;
+        }
+        public void Dispose()
+        {
+            Console.WriteLine(num1 + num2);
+
+        }
+    }
+}
