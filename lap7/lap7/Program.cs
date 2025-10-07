7c9c0e2d2f9769be40761e5d371ccbece53bcde0
nada-aliiiiiiiiiiiiiii <120502506+nada-aliiiiiiiiiiiiiii@users.noreply.github.com>
2025-10-07 19:33:01 +0300

    lap7

---
+++ lap7/lap7/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace lap7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection sqlConnection= new SqlConnection("Data Source=.;Initial Catalog=Company;Integrated Security=True;Trust Server Certificate=True");
+            SqlCommand command = new SqlCommand();
+
+            //Get------------------------------------------
+            command.Connection = sqlConnection;
+            command.CommandText = "select ID,Name from Employee ";
+
+
+            //List<Company> companies = new List<Company>();
+
+            sqlConnection.Open();
+            SqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read()) { 
+                int id = (int) dataReader["ID"];
+                string name = dataReader["Name"].ToString();
+                Console.WriteLine($"{id} , {name}");
+            
+            
+            }
+
+            sqlConnection.Close();
+
+
+            //insert----------------------------------------
+            command.CommandText = "insert into Employee values (100,'Mohamed')";
+            command.Connection = sqlConnection;
+            sqlConnection.Open();
+            int n = command.ExecuteNonQuery();
+            Console.WriteLine(n);
+            sqlConnection.Close();
+
+
+            //delet------------------------------------------
+            command.CommandText = "delet from Employee where ID=1";
+            command.Connection = sqlConnection;
+            sqlConnection.Open();
+            int rowsDeleted = command.ExecuteNonQuery();
+            Console.WriteLine(rowsDeleted);
+            sqlConnection.Close();
+
+            //Update---------------------------------------------
+
+            command.CommandText = "update Employee set Name = 'Nada' where ID = 2";
+            command.Connection = sqlConnection;
+            sqlConnection.Open();
+            int rowsUpdated = command.ExecuteNonQuery();
+            Console.WriteLine(rowsUpdated);
+            sqlConnection.Close();
+
+        }
+    }
+}
