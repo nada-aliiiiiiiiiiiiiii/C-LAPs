7ba9357d3bf22828481e4ef8784be994037266f6
nada-aliiiiiiiiiiiiiii <120502506+nada-aliiiiiiiiiiiiiii@users.noreply.github.com>
2025-10-08 13:14:00 +0300

    LAP8

---
+++ LAP8/LAP8/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace LAP8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Employee", "Data Source=.;Initial Catalog=Company;Integrated Security=True;Trust Server Certificate=True");
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "Employee");
+
+            DataTable dt = ds.Tables["Employee"];
+
+
+
+            //--------------------------- Get ---------------------------
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine($"{row["ID"]}, {row["Name"]}");
+            }
+
+            //--------------------------- Insert ---------------------------
+
+
+            DataRow newRow = dt.NewRow();
+            newRow["ID"] = 300;
+            newRow["Name"] = "Sara";
+            dt.Rows.Add(newRow);
+
+            //--------------------------- Update ---------------------------
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if ((int)row["ID"] == 2)
+                {
+                    row["Name"] = "nada";
+                }
+            }
+
+            //--------------------------- Delete ---------------------------
+
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if ((int)row["ID"] == 1)
+                {
+                    row.Delete();
+                }
+            }
+
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(ds, "Employee");
+
+        }
+    }
+}
